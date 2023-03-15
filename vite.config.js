b63466350a3b71cfc27e69158ac78a8ca8c04b8b
Dani Rodríguez <dani@danirod.es>
2023-03-15 16:56:49 +0100

    fix: entrypoints

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import svelteSvg from "vite-plugin-svelte-svg";
 
 export default defineConfig({
   plugins: [svelte(), svelteSvg()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve('index.html'),
+        stallman: resolve('stallman.html'),
+        countdown: resolve('countdown.html'),
+      }
+    }
+  }
 });
